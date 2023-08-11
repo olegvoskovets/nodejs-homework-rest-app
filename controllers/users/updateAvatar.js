ab1c15101529c8782f8c9ad5354d60279281c035
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-08-11 13:33:51 +0300

    upload file name

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,19 +1,18 @@
 const path = require("path");
-
-const fs = require("fs");
+const fs = require("fs/promises");
 const User = require("../../models/user");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
-  console.log("req.file", req.file);
+  const { _id: id } = req.user;
+  const nameFile = `${id}_${originalname}`;
   try {
-    const resultUpload = path.join(avatarsDir, originalname);
-    console.log("resultUpload", resultUpload);
+    const resultUpload = path.join(avatarsDir, nameFile);
 
     await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("public", "avatars", originalname);
+    const avatarURL = path.join("public", "avatars", nameFile);
 
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
 
