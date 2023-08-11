3058cff39463dbd62278c8866ec3a3437908fbd9
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-08-11 14:46:25 +0300

    update size

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,12 +1,24 @@
 const path = require("path");
 const fs = require("fs/promises");
 const User = require("../../models/user");
+const Jimp = require("jimp");
+
 const avatarDir = path.join(__dirname, "../..", "public", "avatars");
+const tempSize = path.join(__dirname, "../..", "temp");
 
 const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const { _id: id } = req.user;
   const newNameFile = `${id}_${originalname}`;
+  Jimp.read(tempUpload, function (err, test) {
+    if (err) throw err;
+    console.log("tempSize", tempSize);
+    test
+      .resize(250, 250)
+      .quality(50)
+      .write(tempSize + "/" + `${id}_${originalname}`);
+  });
+
   try {
     const resultUpload = path.join(avatarDir, newNameFile);
     await fs.rename(tempUpload, resultUpload);
